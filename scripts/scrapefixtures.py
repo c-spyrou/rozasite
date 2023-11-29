444c8394ce6a97ca33b8760f1be51a6195676cdc
Deano Spyrou <deanospyrou@kubrickgroup.com>
2023-11-29 11:45:28 +0000

    added fixtures and fixture update scraping

--- scripts/scrapefixtures.py
+++ scripts/scrapefixtures.py
@@ -12,8 +12,8 @@ driver = webdriver.Chrome()
 # Send a GET request to the URL using selenium
 driver.get(url)
 
-# Allow some time for the dynamic content to load (adjust as needed)
-time.sleep(15)
+# Allow more time for the dynamic content to load (adjust as needed)
+time.sleep(10)
 
 # Get the page source after dynamic content has loaded
 html_content = driver.page_source
@@ -28,25 +28,17 @@ soup = BeautifulSoup(html_content, 'html.parser')
 div_container = soup.find("div", class_="fixtures-table table-scroll")
 
 if div_container:
-    # Extract data from the div
+    # Extract data from the table
     data = []
-    headers = []
+    max_columns = 0
 
     for row in div_container.find_all("tr"):
-        row_data = []
-        # Find all cells in the row
-        for cell in row.find_all(['td', 'th']):
-            row_data.append(cell.text.strip())
-        
-        if len(row_data) == len(headers) or not headers:
-            if not headers:
-                headers = row_data
-            else:
-                data.append(row_data)
-        else:
-            print(f"Skipping row with unexpected number of columns: {row_data}")
+        row_data = [cell.text.strip() for cell in row.find_all(['td', 'th'])]
+        data.append(row_data)
+        max_columns = max(max_columns, len(row_data))
 
     # Create a DataFrame using the extracted data
+    headers = ["Competition", "Date & Time", "Home", " ", "vs", " ", "Away", "Venue"]
     df = pd.DataFrame(data, columns=headers)
     df = df.dropna()
 
@@ -55,10 +47,76 @@ if div_container:
     file_path = "content/fixtures.html"
     with open(file_path, "a") as html_file:
         # Write the styled HTML to the file
+        html_file.write("<h2 align=center> Upcoming Matches... </h2> <br>")
         html_file.write(fixtures_html)
         html_file.write("\n\n")
 
     print(f"\nStyled DataFrame content appended to the HTML file: {file_path}")
 else:
     print("Div container not found with the specified class.")
+
+
+# import time
+# from selenium import webdriver
+# from bs4 import BeautifulSoup
+# import pandas as pd
+
+# # URL of the website to scrape
+# url = "https://fulltime.thefa.com/displayTeam.html?divisionseason=734243150&teamID=597757996"
+
+# # Set up the selenium webdriver (make sure you have the appropriate webdriver installed)
+# driver = webdriver.Chrome()
+
+# # Send a GET request to the URL using selenium
+# driver.get(url)
+
+# # Allow some time for the dynamic content to load (adjust as needed)
+# time.sleep(15)
+
+# # Get the page source after dynamic content has loaded
+# html_content = driver.page_source
+
+# # Close the webdriver
+# driver.quit()
+
+# # Parse the HTML content
+# soup = BeautifulSoup(html_content, 'html.parser')
+
+# # Find the div with the specified class
+# div_container = soup.find("div", class_="fixtures-table table-scroll")
+
+# if div_container:
+#     # Extract data from the div
+#     data = []
+#     headers = []
+
+#     for row in div_container.find_all("tr"):
+#         row_data = []
+#         # Find all cells in the row
+#         for cell in row.find_all(['td', 'th']):
+#             row_data.append(cell.text.strip())
+        
+#         if len(row_data) == len(headers) or not headers:
+#             if not headers:
+#                 headers = row_data
+#             else:
+#                 data.append(row_data)
+#         else:
+#             print(f"Skipping row with unexpected number of columns: {row_data}")
+
+#     # Create a DataFrame using the extracted data
+#     df = pd.DataFrame(data, columns=headers)
+#     df = df.dropna()
+
+#     fixtures_html = df.to_html(index=False)
+
+#     file_path = "content/fixtures.html"
+#     with open(file_path, "a") as html_file:
+#         # Write the styled HTML to the file
+#         html_file.write(fixtures_html)
+#         html_file.write("\n\n")
+
+#     print(f"\nStyled DataFrame content appended to the HTML file: {file_path}")
+# else:
+#     print("Div container not found with the specified class.")
 
